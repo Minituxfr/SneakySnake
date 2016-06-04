da8c7f00b6cb2bcd8dc034dab9910ef1500d68d4
Michael Demesy <michaeldemesy@gmail.com>
2016-06-05 00:57:35 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,64 @@
+<?php include('head.php'); ?>
+<?php include('nav.php'); ?>
+<title>A propos de Sneaky Snake</title>
+    <!-- Page Content -->
+    <div class="container">
+
+        <hr>
+
+        <div class="row">
+            <div class="col-sm-8">
+                <h2>Ce projet</h2>
+                <p> Ce projet a été réalisé dans le cadre d'un projet tuteuré organisé par l'IUT informatique de Belfort.
+                Il nous a été demandé de développer un jeu à l'aide de Java Swing parmi une sélection.</p>
+                <p>A implémenter pour exposer l'épopée glorieuse que nous avons traversé</p>
+            </div>
+            <div class="col-sm-10">
+                <h2>Qui sommes nous ?</h2>
+                <br/>
+                </div>
+                <div class="row">
+                <div class="col-sm-4 text-center">
+               <img class="img-circle img-responsive center-block" src="img/Platy.jpg" width=200 alt="" id="img-staff">
+                    <a href="AdrienFilippi.php">Adrien FILIPPI</a>
+                    <br>
+                </div>
+                <div class="col-sm-4 text-center">
+               <img class="img-circle img-responsive center-block" src="https://avatars2.githubusercontent.com/u/19168038?v=3&s=460" width=200 alt="" id="img-staff">
+                    <a href="AdrienFilippi.php">Remy RISSER</a>
+                    <br>
+                </div>
+                <div class="col-sm-4 text-center">
+               <img class="img-circle img-responsive center-block" src="https://pbs.twimg.com/profile_images/608235017588764673/guPnbv4i.png" width=200 alt="" id="img-staff">
+                    <a href="AdrienFilippi.php">Alexandre PIZARD</a>
+                    <br>
+                </div>
+                </div>
+                <br>
+                <div class="row">
+               <p></p>
+                <div class="col-sm-4 text-center">
+               <img class="img-circle img-responsive center-block" src="https://avatars0.githubusercontent.com/u/19169278?v=3&s=460" width=200 alt="" id="img-staff">
+                    <a href="AdrienFilippi.php">Michael DEMESY</a>
+                    <br>
+                </div>
+                <div class="col-sm-4 text-center">
+               <img class="img-circle img-responsive center-block" src="https://avatars2.githubusercontent.com/u/19167835?v=3&s=460" width=200 alt="" id="img-staff">
+                    <a href="AdrienFilippi.php">Dylan DELACOTE</a>
+                    <br>
+                </div>
+                <div class="col-sm-4 text-center">
+               <img class="img-circle img-responsive center-block" src="https://avatars0.githubusercontent.com/u/19169357?v=3&s=460" width=200 alt="" id="img-staff">
+                    <a href="AdrienFilippi.php">Hugo DE VITA</a>
+                    <br>
+                </div>
+                
+
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+<?php include('footer.php'); ?>
+
